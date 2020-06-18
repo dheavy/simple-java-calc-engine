27422872358b990cfa6aa0634c7b359382e8b667
Davy Peter Braun <hello@davybraun.com>
2020-06-18 13:18:02 +0200

    Use classes and accessor/mutators

--- src/main/java/com/davybraun/learningjava/calcengine/App.java
+++ src/main/java/com/davybraun/learningjava/calcengine/App.java
@@ -3,34 +3,28 @@ package com.davybraun.learningjava.calcengine;
 /** Hello world! */
 public class App {
   public static void main(String[] args) {
-    double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-    double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-    char[] opCodes = {'d', 'a', 's', 'm'};
-    double[] results = new double[opCodes.length];
+    // double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+    // double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+    // char[] opCodes = {'d', 'a', 's', 'm'};
+    // double[] results = new double[opCodes.length];
+    MathEquation[] equations = new MathEquation[4];
+    equations[0] = create(100.0d, 50.0d, 'd');
+    equations[1] = create(25.0d, 92.0d, 'a');
+    equations[2] = create(255.0d, 17.0d, 's');
+    equations[3] = create(11.0d, 3.0d, 'm');
 
-    for (int i = 0; i < opCodes.length; i++) {
-      switch (opCodes[i]) {
-        case 'a':
-          results[i] = leftVals[i] + rightVals[i];
-          break;
-        case 's':
-          results[i] = leftVals[i] - rightVals[i];
-          break;
-        case 'd':
-          results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0;
-          break;
-        case 'm':
-          results[i] = leftVals[i] * rightVals[i];
-          break;
-        default:
-          System.out.println("Error - invalid opCode");
-          results[i] = 0.0d;
-      }
-    }
-
-    for (double res : results) {
+    for (MathEquation eq : equations) {
+      eq.execute();
       System.out.print("result = ");
-      System.out.println(res);
+      System.out.println(eq.getResult());
     }
+  }
+
+  public static MathEquation create(double leftVal, double rightVal, char opCode) {
+    MathEquation equation = new MathEquation();
+    equation.setLeftVal(leftVal);
+    equation.setRightVal(rightVal);
+    equation.setOpCode(opCode);
+    return equation;
   }
 }
